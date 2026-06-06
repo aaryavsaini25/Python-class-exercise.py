be6cdea1e00dee0aefb1f65f7fc287a65abd8e34
Aaryav <aaryav.saini25@gmail.com>
2026-06-06 13:18:48 +0100

    Created file for the first excercise!

---
+++ multiply-or-add.py
@@ -0,0 +1,8 @@
+def multipyoradd(a,b):
+    c=a*b
+    d=a+b
+    if a*b<=1000:
+        print(f"{a} x {b} = {c}")
+    else:
+        print(f"{a} + {b} = {d}")
+multipyoradd(int(input("Enter your first number:")),int(input("Enter your first number:")))
